48a85f897f57885d95bd26b4025e85de1e5744ed
Kaliane <vankaliane@gmail.com>
2023-04-28 14:17:44 -0400

    Added 'two_sum' function to 'two_sum.py' file and added tests to 'two_sum_test.py' file

--- tests/two_sum_test.py
+++ tests/two_sum_test.py
@@ -1,2 +1,25 @@
 import pytest
 from functions.two_sum import twoSum
+
+def test_returns_1_2_for_3_2_4_with_target_6():
+    # Arrange
+    n = [3, 2, 4]
+    t = 6
+
+    # Act
+    answer = twoSum(n, t)
+
+    # Assert
+    assert answer == [1, 2]
+
+
+def test_returns_0_1_for_3_3_with_target_6():
+    # Arrange
+    n = [3, 3]
+    t = 6
+
+    # Act
+    answer = twoSum(n, t)
+
+    # Assert
+    assert answer == [0, 1]
